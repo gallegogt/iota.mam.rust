85c7a9a44b1cb8a421faedd1f7251f9882be94cb
Yolier Galan Tasse <gallegogt@gmail.com>
2019-05-08 22:26:54 -0400

    Add serialize functions to mam/prng

--- src/mam_prng.rs
+++ src/mam_prng.rs
@@ -1,12 +1,16 @@
-use ffi;
-use std::mem;
-
+use crate::errors::{MamError, MamResult};
 use crate::trits::Trits;
+use ffi;
 
+pub enum PrngDst {
+    SecKey = 0,
+    WotsKey = 1,
+    NtruKey = 2,
+}
 
 #[derive(Clone)]
 pub struct MamPrng {
-    pub c_prng: Box<ffi::mam_prng_t>
+    pub c_prng: ffi::mam_prng_t,
 }
 
 impl MamPrng {
@@ -16,73 +20,213 @@ impl MamPrng {
     pub fn new(secret_key: Trits) -> Self {
         unsafe {
             let mut sf = Self {
-                c_prng: Box::new(ffi::mam_prng_t{
-                    secret_key: mem::uninitialized(),
-                })
+                c_prng: ffi::mam_prng_t {
+                    secret_key: [0; ffi::MAM_PRNG_SECRET_KEY_SIZE as usize],
+                },
             };
 
-            ffi::mam_prng_init(&mut *(sf.c_prng), secret_key.into_raw());
-
+            ffi::mam_prng_init(&mut sf.c_prng, secret_key.into_raw());
             sf
         }
     }
 
-    // /**
-    //  * PRNG output generation with a nonce
-    //  *
-    //  * @param prng A PRNG interface
-    //  * @param destination A destination tryte
-    //  * @param nonce The nonce
-    //  * @param output Pseudorandom output trits
-    //  */
-    pub fn gen(&self, destination: u32, nonce: &Trits, output: &mut Trits) {
+    ///
+    /// Return the C raw info
+    ///
+    pub fn into_raw(&self) -> ffi::mam_prng_t {
+        self.c_prng
+    }
+
+    ///
+    /// PRNG output generation with a nonce
+    ///
+    /// @param prng A PRNG interface
+    /// @param destination A destination tryte
+    /// @param nonce The nonce
+    /// @param output Pseudorandom output trits
+    ///
+    pub fn gen(&self, destination: PrngDst, nonce: &Trits, output: &Trits) -> MamResult<()> {
+        self.gen3(destination, nonce, &Trits::null(), &Trits::null(), output)
+    }
+
+    ///
+    /// @brief Generates pseudo random trits with three nonces
+    ///  *
+    /// @param[in] prng A PRNG
+    /// @param[in] destination A destination tryte
+    /// @param[in] nonce1 The first nonce
+    /// @param[in] nonce2 The second nonce
+    /// @param[in] nonce3 The third nonce
+    /// @param[out] output The pseudo random trits
+    ///  *
+    /// @return a status code
+    ///  */
+    pub fn gen3(
+        &self,
+        destination: PrngDst,
+        nonce1: &Trits,
+        nonce2: &Trits,
+        nonce3: &Trits,
+        output: &Trits,
+    ) -> MamResult<()> {
         unsafe {
-            ffi::mam_prng_gen(&(*self.c_prng), destination, nonce.into_raw(), output.c_trits)
+            let rc = ffi::mam_prng_gen3(
+                &self.c_prng,
+                destination as u32,
+                nonce1.into_raw(),
+                nonce2.into_raw(),
+                nonce3.into_raw(),
+                output.into_raw(),
+            );
+
+            if rc != ffi::retcode_t_RC_OK {
+                return Err(MamError::from(rc));
+            }
+
+            Ok(())
         }
     }
 
-}
+    ///
+    /// @brief Generates pseudo random trits with two nonces
+    ///
+    /// @param[in] prng A PRNG
+    /// @param[in] destination A destination tryte
+    /// @param[in] nonce1 The first nonce
+    /// @param[in] nonce2 The second nonce
+    /// @param[out] output The pseudo random trits
+    ///
+    /// @return a status code
+    ///
+    pub fn gen2(
+        &self,
+        destination: PrngDst,
+        nonce1: &Trits,
+        nonce2: &Trits,
+        output: &Trits,
+    ) -> MamResult<()> {
+        self.gen3(destination, nonce1, nonce2, &Trits::null(), output)
+    }
 
-impl Drop for MamPrng {
+    ///
+    /// Size of a serialized PRNG
+    ///
+    pub fn serialize_size() -> usize {
+        ffi::MAM_PRNG_SECRET_KEY_SIZE as usize
+    }
 
-    /// PRNG deinitialization
-    fn drop(&mut self) {
+    ///
+    ///  Serializes a PRNG into a trits buffer
+    ///
+    pub fn serialize(&self) -> Trits {
         unsafe {
-            ffi::mam_prng_destroy(&mut (*self.c_prng));
-        }
+            let mut buffer = Trits::new(MamPrng::serialize_size());
+            buffer.set_zero();
 
+            let mut from_rep = Trits::from((
+                ffi::MAM_PRNG_SECRET_KEY_SIZE as usize,
+                self.c_prng.secret_key.as_ptr(),
+            ));
+
+            ffi::pb3_encode_ntrytes(from_rep.into_raw(), buffer.mut_into_raw());
+
+            from_rep.set_null();
+
+            buffer.pickup_all()
+        }
+    }
+    ///
+    /// Deserializes a PRNG from a trits buffer
+    ///
+    /// @param[in] buffer The trits buffer
+    ///
+    pub fn deserialize(buffer: &mut Trits) -> MamResult<MamPrng> {
+        unsafe {
+            let c_prng = ffi::mam_prng_t {
+                secret_key: [0; ffi::MAM_PRNG_SECRET_KEY_SIZE as usize],
+            };
+
+            let mut trits = Trits::from((
+                ffi::MAM_PRNG_SECRET_KEY_SIZE as usize,
+                c_prng.secret_key.as_ptr(),
+            ));
+
+            let rc = ffi::pb3_decode_ntrytes(trits.into_raw(), buffer.mut_into_raw());
+
+            // TODO: Fix error of no alloc memory
+            trits.set_null();
+
+            if rc != ffi::retcode_t_RC_OK {
+                return Err(MamError::from(rc));
+            }
+
+            Ok(MamPrng { c_prng: c_prng })
+        }
     }
 }
-
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::error::Error;
 
     #[test]
     fn check_new_mam_prng() {
-        let mut key = Trits::new(ffi::MAM_PRNG_KEY_SIZE as usize);
+        let mut key = Trits::new(ffi::MAM_PRNG_SECRET_KEY_SIZE as usize);
         key.set_zero();
-        key.from_str("NOPQRSTUVWXYZ9ABCDEFGHIJKLMNOPQRSTUVWXYZ9ABCDEFGHIJKLMNOPQRSTUVWXYZ9ABCDEFGHIJKLM");
-
-        let mut nonce = Trits::new(18);
+        key.from_str(
+            "NOPQRSTUVWXYZ9ABCDEFGHIJKLMNOPQRSTUVWXYZ9ABCDEFGHIJKLMNOPQRSTUVWXYZ9ABCDEFGHIJKLM",
+        );
+        let nonce = Trits::new(18);
         nonce.set_zero();
-
-        let mut y1 = Trits::new(243 * 2 + 18);
-        y1.set_zero();
-
-        let mut y2 = Trits::new(243 * 2 + 18);
+        let y2 = Trits::new(243 * 2 + 18);
         y2.set_zero();
-
-        println!("{:?} \n {:?}", key.size(), key.to_str());
+        let y1 = Trits::new(243 * 2 + 18);
+        y1.set_zero();
         let prng = MamPrng::new(key);
-        prng.gen(0, &nonce, &mut y1);
-        prng.gen(1, &nonce, &mut y2);
 
-        println!("{:?} \n {:?}", y1.size(), y1.to_str());
-        println!("{:?} \n {:?}", y2.size(), y2.to_str());
+        match prng.gen(PrngDst::SecKey, &nonce, &y2) {
+            Err(e) => {
+                println!("{:?}", e.description());
+            }
+            Ok(_) => {}
+        }
 
-        assert_eq!(30, 30);
+        match prng.gen(PrngDst::NtruKey, &nonce, &y1) {
+            Err(e) => {
+                println!("{:?}", e.description());
+            }
+            Ok(_) => {}
+        }
+
+        assert_ne!(y1, y2);
+    }
+
+    #[test]
+    fn check_prng_serialization() {
+        let mut key = Trits::new(ffi::MAM_PRNG_SECRET_KEY_SIZE as usize);
+        key.set_zero();
+        key.from_str(
+            "NOPQRSTUVWXYZ9ABCDEFGHIJKLMNOPQRSTUVWXYZ9ABCDEFGHIJKLMNOPQRSTUVWXYZ9ABCDEFGHIJKLM",
+        );
+        let prng1 = MamPrng::new(key);
+        let mut trits = prng1.serialize();
+
+        match MamPrng::deserialize(&mut trits) {
+            Err(e) => {
+                println!("Error {:?}", e);
+                assert!(false);
+            }
+
+            Ok(prng2) => {
+                // TODO: Fix error of no alloc memory
+                trits.set_null();
+
+                assert_eq!(
+                    prng1.into_raw().secret_key.len(),
+                    prng2.into_raw().secret_key.len()
+                );
+            }
+        };
     }
 }
-

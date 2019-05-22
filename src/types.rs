216f8d51338b28c96eff244a282f2d050c2b8595
Yolier Galan Tasse <gallegogt@gmail.com>
2019-05-21 21:17:05 -0400

    Add mod types

---
+++ src/types.rs
@@ -0,0 +1,26 @@
+use ffi;
+
+///
+/// Tryte
+///
+pub type Tryte = ffi::tryte_t;
+///
+///   Signed integer type capable of storing 6 trits
+///
+pub type Trint6 = ffi::trint6_t;
+///
+///   Signed integer type capable of storing 9 trits
+///
+pub type Trint9 = ffi::trint9_t;
+///
+///   Signed integer type capable of storing 18 trits
+///
+pub type Trint18 = ffi::trint18_t;
+///
+///   Signed integer type capable of storing 18 trits
+///
+pub type Byte = ffi::byte;
+///
+/// Trit
+///
+pub type Trit = ffi::trit_t;

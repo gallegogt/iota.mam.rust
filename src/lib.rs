6d244d7c5cb5878b47df01162954bf69d7e19a0e
Yolier Galan Tasse <gallegogt@gmail.com>
2019-07-31 17:15:00 -0400

    Re-design project

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,3 @@
-#[macro_use]
-extern crate log;
-extern crate libc;
-
-extern crate iota_mam_sys as ffi;
-
-mod api;
-mod bundle;
-mod channel;
-mod constants;
-pub mod converter;
-mod endpoint;
-mod errors;
-mod mss;
-mod ntru;
-mod prng;
-mod psk;
-mod spongos;
-mod trits;
-mod types;
-
-pub use channel::*;
-pub use endpoint::*;
-pub use errors::*;
-pub use mss::*;
-pub use prng::*;
-pub use spongos::*;
-pub use trits::*;
-// pub use message::*;
-pub use api::*;
-pub use bundle::*;
-pub use constants::*;
-pub use ntru::*;
-pub use psk::*;
-pub use types::*;
 
 #[cfg(test)]
 mod tests {
